75873f7c95019807851ee44aa8baf4a8823a0519
Hodoronk <tanasa.andrei@hotmail.com>
2023-10-13 15:50:28 +0300

    added pop() method which uses a crawl recursive function to find this.tail and reassign it

--- linkedList.js
+++ linkedList.js
@@ -1,35 +1,35 @@
 class LinkedList {
     constructor() {
-        this.head = new Node(null);
-        this.end = new Node();
-        this.size = 0;
-        this.tail = null;
+        this.head = new Node(null)
+        this.end = new Node()
+        this.size = 0
+        this.tail = null
         
     }
 
     prepend(value) {
         if (this.head.next === this.end) {
-            const newNode = new Node(value, this.end);
-            this.head.next = newNode;
-            this.size++;
+            const newNode = new Node(value, this.end)
+            this.head.next = newNode
+            this.size++
         } else {
-            const newNode = new Node(value, this.head.next);
-            this.head.next = newNode;
-            this.size++;
+            const newNode = new Node(value, this.head.next)
+            this.head.next = newNode
+            this.size++
         }
     }
 
     append(value) {
         if(this.head.next === this.end){
-            const newNode = new Node(value, this.end);
-            this.head.next = newNode; 
+            const newNode = new Node(value, this.end)
+            this.head.next = newNode;
             this.size++;
         } else {
-            const lastElement = findLast(this.head) ;
-            const newNode = new Node(value);
-            lastElement.next = newNode;
-            newNode.next = null;
-            this.size++;
+            const lastElement = findLast(this.head) 
+            const newNode = new Node(value)
+            lastElement.next = newNode
+            newNode.next = null
+            this.size++
         }
     }
 
@@ -40,70 +40,87 @@ class LinkedList {
         let indexCount = -1;
         const assignIndices = (element) => {
             if(element.next == null) {
-                element.index = indexCount;
+                element.index = indexCount
                 return
             } else {
-                element.index = indexCount;
+                element.index = indexCount
                 indexCount++;
-                assignIndices(element.next) ;
+                assignIndices(element.next) 
             }
         }
 
         const findIndex = (element, index) => {
             if(element.index === index) {
-                return element.value;
+                return element.value
             } else{
-                return findIndex(element.next, index);
+                return findIndex(element.next, index)
             }
         }
         assignIndices(this.head);
         if(index > indexCount) {
-            console.log(`Index too large`) ;
+            console.log(`Index too large`) 
         } else {
 
-            console.log(`Element at index ${index} is ${findIndex(this.head, index)}`) ;
+            console.log(`Element at index ${index} is ${findIndex(this.head, index)}`) 
         }
     }
+    pop(){
+        const crawler = (element) => {
+            if(element.next == this.tail) {
+                this.size --
+                this.tail = element;
+                element.next = this.end
+                return
+            } else {
+                crawler(element.next) 
+            }
+        }
+        crawler(this.head) 
+    }
+
 }
 
 const findLast = (element) => {
-    if (element.next == null) {
+    if (element.next == this.end) {
         console.log(`RETURNED ELEMENT: ${element.value}` ) ;
-        linkedList.tail = element;
-        return element;
+        linkedList.tail = element
+        return element
     } else {
         console.log(`element value : ${element.value}`);
-        return findLast(element.next);
+        return findLast(element.next)
     }
 }
 
 class Node {
     constructor(value = null, next = null, index = null) {
-        this.value = value;
-        this.next = next;
-        this.index = index;
+        this.value = value
+        this.next = next
+        this.index = index
     }
 }
 
-const linkedList = new LinkedList();
+const linkedList = new LinkedList()
 
-const val = '1';
-const val2 = '2';
-const val3 = '3';
-const val4 = '4';
-const val5 = '5' ;
-const val6 = '6' ;
-const val7 = '7'; 
-linkedList.prepend(val);
-linkedList.prepend(val2);
-linkedList.prepend(val3);
-linkedList.prepend(val4);
-linkedList.append(val5);
-linkedList.append(val6);
-linkedList.append(val7);
+const val = '1'
+const val2 = '2'
+const val3 = '3'
+const val4 = '4'
+const val5 = '5' 
+const val6 = '6' 
+const val7 = '7'
+linkedList.prepend(val)
+linkedList.prepend(val2)
+linkedList.prepend(val3)
+linkedList.prepend(val4)
+linkedList.append(val5)
+linkedList.append(val6)
+linkedList.append(val7)
 
-findLast(linkedList.head);
-linkedList.listSize();
-linkedList.firstElement();
-linkedList.lastElement();
-linkedList.at(6) ;
+findLast(linkedList.head)
+linkedList.listSize()
+linkedList.firstElement()
+linkedList.lastElement()
+linkedList.at(6) 
+
+
+linkedList.pop();
